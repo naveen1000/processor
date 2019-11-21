3faee0c76d81544cc0289a189ab6dc6dd9753f36
naveen1000 <loki.naveen1000@gmail.com>
2019-11-21 14:31:55 +0530

    executed add & sub successfully using code

--- tb_main.v
+++ tb_main.v
@@ -26,12 +26,12 @@ $monitor ($time," A=%d, B=%d,alu_out=%d,sel=%d,ir_data=%b",A,B,alu_out,addr,ir_d
     begin
         #5 pc=i;en=1;
         #5 $display("%b",ir_data[15:12]);
-        if(ir_data[15:12]==4'b1000)
+        if(ir_data[15:12]==4'b1000) //load 
         begin
             addr=ir_data[9:8]; wr=1; data_in=ir_data[7:0];
             #5 wr=0;
         end
-        if(ir_data[15:12]==4'b0000)
+        if(ir_data[15:12]==4'b0000) //add
         begin
         addr=ir_data[5:4]; rd=1;
         #5 A=data_out;
@@ -43,6 +43,20 @@ $monitor ($time," A=%d, B=%d,alu_out=%d,sel=%d,ir_data=%b",A,B,alu_out,addr,ir_d
         #5 addr=ir_data[9:8]; wr=1; data_in=alu_out;
         #5 wr=0; 
         end
+        if(ir_data[15:12]==4'b0001) //sub
+        begin
+        addr=ir_data[5:4]; rd=1;
+        #5 A=data_out;
+        addr=ir_data[1:0]; rd=1;
+        #5 B=data_out;
+        #5 $display("opcode=%b",ir_data[14:12]);
+        #5 opcode = ir_data[14:12];
+        $display("sub executed");
+        #5 addr=ir_data[9:8]; wr=1; data_in=alu_out;
+        #5 wr=0; 
+        end
+
+
     end
 
 #5 $finish;
