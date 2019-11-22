d379831fa2163aa002d5e255087ee67522e4a577
naveen1000 <loki.naveen1000@gmail.com>
2019-11-22 17:21:20 +0530

    added cy zero flags to alu and verified

--- alu.v
+++ alu.v
@@ -1,22 +1,24 @@
-module alu(opcode,A,B,alu_out);
+module alu(opcode,A,B,alu_out,cy,zero);
 input [2:0]opcode;
 input [7:0]A,B;
 output reg [7:0]alu_out;
-
+output reg  cy,zero;
 
 always @(opcode,A,B) 
 begin
 case(opcode)
-0: alu_out <= A + B;
-1: alu_out <= A - B;
-2: alu_out <= A & B;
-3: alu_out <= A | B;
-4: alu_out <= A ^ B;
-5: alu_out <= ~A;
-6: alu_out <= A>>B;
-7: alu_out <= A<<B;
-default: alu_out <= 0; 
+0: begin{cy,alu_out} = A + B;zero=(alu_out==8'b0);end//blocking
+1: begin{cy,alu_out} = A - B;zero=(alu_out==8'b0);end
+2: begin{cy,alu_out} = A & B;zero=(alu_out==8'h00);end
+3: begin{cy,alu_out} = A | B;zero=(alu_out==8'h00);end
+4: begin{cy,alu_out} = A ^ B;zero=(alu_out==8'h00);end
+5: begin{cy,alu_out} = ~A;zero=(alu_out==8'h00);end
+6: begin{cy,alu_out} = A>>B;zero=(alu_out==8'h00);end
+7: begin{cy,alu_out} = A<<B;zero=(alu_out==8'h00);end
+default: begin{cy,alu_out} = 0;zero=(alu_out==8'h00);end 
 endcase
+
+
 //$display($time," ALU: %b",ALUOut);
 //$display($time, " Zero: %b", (ALUOut==0));
 end
--- alu_tb.v
+++ alu_tb.v
@@ -2,14 +2,15 @@ module alu_tb;
 reg [2:0]opcode;
 reg [7:0]A,B;
 wire [7:0]alu_out;
+wire cy,zero,parity;
 integer i;
-alu DUT(opcode,A,B,alu_out);
+alu DUT(opcode,A,B,alu_out,cy,zero);
 initial
 begin
-$monitor ($time," A=%d, B=%d,out=%d",A,B,alu_out);
+$monitor ($time," A=%d, B=%d,out=%d cy=%d zero=%d  op=%d",A,B,alu_out,cy,zero,opcode);
     for (i=0; i<8; i=i+1)
     begin
-    #5 A=8'h02; B=8'h01; opcode=i;
+    #5 A=8'h00; B=8'h01; opcode=i;
     end
 #5 $finish;
 end
